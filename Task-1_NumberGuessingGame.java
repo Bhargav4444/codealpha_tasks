54b6a8dd9a4a362ae66d375ce02dfcdb817b3a70
Bhargav4444 <81967795+Bhargav4444@users.noreply.github.com>
2023-10-21 21:24:25 +0530

    Update Task-1_NumberGuessingGame.java

--- Task-1_NumberGuessingGame.java
+++ Task-1_NumberGuessingGame.java
@@ -1,25 +1,29 @@
 import java.util.Random;
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class NumberGuessingGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int randomNumber = random.nextInt(100) + 1; 
+        int randomNumber = random.nextInt(100) + 1;
         int attempts = 0;
-        boolean hasGuessedCorrectly = false;
+        boolean guessedCorrectly = false;
 
-        while (!hasGuessedCorrectly) {
-            String input = JOptionPane.showInputDialog("Guess the number between 1 to 100:");
-            int userGuess = Integer.parseInt(input);
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess the number between 1 to 100.");
+
+        while (!guessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
             attempts++;
 
             if (userGuess == randomNumber) {
-                hasGuessedCorrectly = true;
-                JOptionPane.showMessageDialog(null, "Congratulations! You guessed the number in " + attempts + " attempts.");
+                guessedCorrectly = true;
+                System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
             } else if (userGuess < randomNumber) {
-                JOptionPane.showMessageDialog(null, "Too low! Try again.");
+                System.out.println("Too low! Try again.");
             } else {
-                JOptionPane.showMessageDialog(null, "Too high! Try again.");
+                System.out.println("Too high! Try again.");
             }
         }
     }
